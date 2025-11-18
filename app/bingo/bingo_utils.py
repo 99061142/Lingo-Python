1e4dc7a15410314038ee55b8fdec7ca5b3b378af
99061142 <70692479+99061142@users.noreply.github.com>
2025-11-18 14:49:26 +0100

    Add functions to generate randomized bingo boards
    * Add get_random_number to select unique even/odd numbers based on team ID.
    
    * Add create_randomized_bingo_board to generate a 4x4 bingo board with randomized numbers for each team.
    
    The create_randomized_bingo_board is being used to initialize the teams data which can be seen at the previous commit.

--- app/bingo/bingo_utils.py
+++ app/bingo/bingo_utils.py
@@ -1,2 +1,35 @@
-from .words import five_letter_words
 from ..teamsData import teamsData
+from random import choice
+from typing import List
+
+"""
+    Returns a random number between 1 and 100 that is not in the unavailableNumbers list.
+"""
+def get_random_number(teamID: int, unavailableNumbers: List[int] = []) -> int:
+    start = 1
+    end = 100
+
+    # Ensure that the team with the ID of 0 gets even numbers, and the team with the ID of 1 gets odd numbers
+    while True:
+        number = choice(range(start, end + 1))
+        if number % 2 == teamID and number not in unavailableNumbers:
+            return number
+
+"""
+    Returns a 4x4 bingo board with randomized numbers for the given teamID.
+    The teamID can only be 0 or 1 in our current implementation.
+"""
+def create_randomized_bingo_board(teamID: int) -> List[List[int]]:
+    unavailableNumbers = [] # Numbers the team already has on their board
+    board_size = 4 # This is the amount of rows AND columns
+    bingo_board = []
+
+    for _ in range(board_size):
+        bingo_row = []
+        for _ in range(board_size):
+            number = get_random_number(teamID, unavailableNumbers)
+            unavailableNumbers.append(number)
+            bingo_row.append(number)
+        bingo_board.append(bingo_row)
+
+    return bingo_board
