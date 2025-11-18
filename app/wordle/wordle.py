3da0712d0090189b433b475f2fbbfa263f723829
99061142 <70692479+99061142@users.noreply.github.com>
2025-11-18 21:21:37 +0100

    Refactor and expand Wordle game logic
    * Added `current_row` as `ask_user_guess` parameter to make it more clear for what row the user is guessing.
    
    * Improve error handling when the user makes a wrong guess (e.g. more specific error message).
    
    * Show the Wordle board when the user starts the game, and let the user guess the word.
    
    This is the start of the game logic and implementation. The game, and so, adding the guess to the board, doesn't work yet.

--- app/wordle/wordle.py
+++ app/wordle/wordle.py
@@ -1,9 +1,42 @@
+from app.app_utils import print_message
 from .wordle_utils import *
 
-def ask_user_guess() -> str:
+"""
+    Let the user input their guess.
+"""
+def ask_user_guess(current_row: int) -> str:
     while True:
-        guess = input("Enter your 5-letter guess: ").strip().lower()
-        if is_valid_guess(guess):
+        guess = input(f"Enter your 5-letter guess for row {current_row + 1}: ").strip().lower()
+        guess_validation = is_valid_guess(guess)
+
+        if guess_validation["isValid"]:
             return guess
-        else:
-            print("Invalid word. Please try again.")
+        
+        error_message = guess_validation["message"]
+        print_color = "red"
+        print_message(error_message, print_color)
+
+""""
+    Print the current Wordle board for the specified team.
+"""
+def show_wordle_board(current_team_ID: int, max_attempts: int, word_to_guess_length: int, word_to_guess: str) -> None:
+    stringified_wordle_board = get_stringified_wordle_board(current_team_ID, max_attempts, word_to_guess_length, word_to_guess)
+    print(stringified_wordle_board)
+
+"""
+    Main function to play the Wordle game.
+"""
+def play_wordle() -> None:
+    max_attempts = 5
+    word_to_guess = get_random_word()
+    word_to_guess_length = len(word_to_guess)
+
+    current_team_ID = 0 
+
+    while True:
+        for row in range(max_attempts):
+            show_wordle_board(current_team_ID, max_attempts, word_to_guess_length, word_to_guess)
+            
+            user_guess = ask_user_guess(row)
+
+        current_team_ID = (current_team_ID + 1) % len(teamsData) # Switch to the next team. If we exceed the number of teams, go back to team 0.
