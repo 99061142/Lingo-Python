c2d44806d33e5c5946b73e142d0e06d09f51f307
99061142 <70692479+99061142@users.noreply.github.com>
2025-11-18 18:46:34 +0100

    Add get_random_word to get random 5 letter word
    * Add get_random_word function to return a random five-letter word that hasn't been used before. We use use a global set to track unavailable words and prevent repeats.

--- app/wordle/wordle_utils.py
+++ app/wordle/wordle_utils.py
@@ -1,2 +1,19 @@
 from ..teamsData import teamsData
 from .words import five_letter_words
+from random import choice
+
+# Set to keep track of unavailable words.
+# This will be used to avoid repeating words in the game.
+_unavailable_words = set()
+
+"""
+    Returns a random five-letter word from the list of words.
+"""
+def get_random_word() -> str:
+    words = five_letter_words.words
+
+    while True:
+        word = choice(words)
+        if word not in _unavailable_words:
+            _unavailable_words.add(word)
+            return word
