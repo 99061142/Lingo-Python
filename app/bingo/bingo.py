3f3cbe24c7e12a8c0a279a0f15fce314359a6fd6
99061142 <70692479+99061142@users.noreply.github.com>
2025-11-20 16:31:25 +0100

    Added function to print the bingo board

--- app/bingo/bingo.py
+++ app/bingo/bingo.py
@@ -1,1 +1,9 @@
+from app.app_utils import print_message
 from .bingo_utils import *
+
+"""
+    Print the bingo board for the specified team.
+"""
+def print_bingo_board_for_team(team_ID: int) -> None:
+    stringified_bingo_board = get_stringified_bingo_board_for_team(team_ID)
+    print_message(stringified_bingo_board)
