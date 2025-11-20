549ed791e61a557a0b0e972608567431926ef60a
99061142 <70692479+99061142@users.noreply.github.com>
2025-11-20 15:45:18 +0100

    Added function to restart the game

--- app/app.py
+++ app/app.py
@@ -1,6 +1,15 @@
 from .app_utils import initialize_teams_data
 from .wordle.wordle import play_wordle
 
+"""
+    Starts the game by initializing teams data and starting the Wordle game.
+"""
 def start_game() -> None:
     initialize_teams_data()
     play_wordle()
+
+"""
+    Restart the game by re-initializing the teams data and starting the game.
+"""
+def restart_game() -> None:
+    start_game()
