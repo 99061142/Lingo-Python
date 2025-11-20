54ced761ab9066caf2ca3dc5be417efbbbf4b04b
99061142 <70692479+99061142@users.noreply.github.com>
2025-11-20 15:50:41 +0100

    Added function which asks user to play again
    Added function which asks the user if they want to play again.
    
    If they do, we restart the game.
    If not, the game ends.
    
    If the user fills in an input which isn't an option, we show an error message.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from .app_utils import initialize_teams_data
+from .app_utils import initialize_teams_data, print_message
 from .wordle.wordle import play_wordle
 
 """
@@ -13,3 +13,32 @@ def start_game() -> None:
 """
 def restart_game() -> None:
     start_game()
+
+"""
+    Ask the user if they want to play again after the game ends.
+    If they do, we restart the game.
+    If not, we print a thank you message.
+"""
+def ask_to_play_again() -> None:
+    # Valid user input options
+    valid_options = {
+        'yes': ['yes', 'y'],
+        'no': ['no', 'n']
+    }
+    
+    while True:
+        user_input = input("Do you want to play again? (yes/no): ").strip().lower()
+        
+        # If they user wants to play again
+        if user_input in valid_options['yes']:
+            restart_game()
+            return
+        
+        # If the user does not want to play again
+        if user_input in valid_options['no']:
+            print_message("Thank's for playing the game!'")
+            return
+        
+        error_message = "Invalid input. Please enter 'yes' or 'no'."
+        error_message_color = "red"
+        print_message(error_message, error_message_color)
