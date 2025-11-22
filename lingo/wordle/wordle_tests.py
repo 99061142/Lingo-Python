f57759e0a519e7a7c775add4513db2072e5daffc
99061142 <70692479+99061142@users.noreply.github.com>
2025-11-22 07:43:27 +0100

    Add test for get_random_word exception handling
    * Added test to verify that get_random_word raises an exception when all available words have been used.

--- lingo/wordle/wordle_tests.py
+++ lingo/wordle/wordle_tests.py
@@ -1,7 +1,32 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
-from test_lib import report
+from lingo.wordle.wordle_utils import get_random_word
+from test_lib import report, test
 
+"""
+    Test whether the function which would return a random word raises an exception when all available words have been used.
+"""
+def test_random_word_error_handling():
+    words = [
+        "test_word"
+    ]
+    used_words = set()
+
+    for _ in range(len(words)):
+        get_random_word(words, used_words)
+
+    try:
+        get_random_word(words, used_words)
+    except Exception as error:
+        test(
+            "Getting a random word when all words have been used should raise an exception.",
+            "All available Wordle words have been used.",
+            str(error),
+        )
+test_random_word_error_handling()
+
+
+# Run the report when this file is executed directly instead of within the tests.py file
 if __name__ == "__main__":
     report()
