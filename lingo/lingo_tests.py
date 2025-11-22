93e484b37402c59178a7c717a16e5f8a92a34108
99061142 <70692479+99061142@users.noreply.github.com>
2025-11-22 06:15:04 +0100

    Added initial test files
    Added the initial library to let us test the application.
    
    We added the initial test files for whole game itself (lingo_tests), wordle, and bingo.
    
    If we want to test the whole game, we need to run tests.py

---
+++ lingo/lingo_tests.py
@@ -0,0 +1,7 @@
+# Run all lingo-related tests
+import lingo.bingo.bingo_tests
+
+# Run all wordle-related tests
+import lingo.wordle.wordle_tests
+
+# Run all lingo-related tests
---
+++ tests.py
@@ -0,0 +1,5 @@
+import lingo.lingo_tests
+from test_lib import report
+
+# Report the test results after every test has been run
+report()
